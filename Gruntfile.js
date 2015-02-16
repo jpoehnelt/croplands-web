901492e0742022dc58b1133453ac8af3de842fc1
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-02-15 22:24:10 -0700

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,107 @@
+/*global module:false*/
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            '*/\n',
+        // Task configuration.
+        concat: {
+            options: {
+                separator: ';\n'
+            },
+            dist: {
+                src: ['app/js/lib/*.js', 'app/js/utilities/*.js', 'app/js/app/config.js', 'app/js/services/*.js', 'app/js/filters/*.js', 'app/js/controllers/*.js', 'app/js/directives/*.js'  ],
+                dest: 'app/js/app.js'
+            }
+        },
+        uglify: {
+            options: {
+                mangle: true,
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'app/js/app.min.js'
+            }
+        },
+        less: {
+            development: {
+                options: {
+                },
+                files: {
+                    // target.css file: source.less file
+                    "app/css/main.<%= pkg.version %>.css": "app/css/less/bootstrap.less"
+                }
+            },
+            production: {
+                options: {
+                    cleancss: true,
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    // target.css file: source.less file
+                    "app/css/main.<%= pkg.version %>.min.css": "app/css/less/bootstrap.less"
+                }
+            }
+        },
+
+        watch: {
+            js: {
+                files: ['<%= concat.dist.src %>', 'Gruntfile.js', 'package.json'],
+                tasks: ['concat', 'uglify']
+            },
+            less: {
+                files: ['app/css/less/*', 'app/css/less/mixins/*', 'Gruntfile.js', 'package.json'],
+                tasks: ['less']
+            },
+            //run unit tests with karma (server needs to be already running)
+            karma: {
+                files: ['app/js/*', 'tests/*.js'],
+                tasks: ['karma:unit:run'] //NOTE the :run flag
+            }
+        },
+        sloc: {
+            'source': {
+                files: {
+                    './': ['app/js/app/*.js',
+                        'app/js/controllers/*.js',
+                        'app/js/directives/*.js',
+                        'app/js/filters/*.js',
+                        'app/js/services/*.js',
+                        'app/partials/*.html',
+                        'gfsad/templates/*.html',
+                        'app/css/app_v2.css']
+                }
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                background: true,
+                singleRun: false
+            },
+            continuous: {
+                configFile: 'karma.conf.js',
+                singleRun: true
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-sloc');
+    grunt.loadNpmTasks('grunt-karma');
+
+    // Default task.
+    grunt.registerTask('default', ['concat', 'uglify', 'less', 'sloc']);
+
+};
---
+++ karma.conf.js
@@ -0,0 +1,78 @@
+// Karma configuration
+// Generated on Sun Feb 15 2015 21:34:54 GMT-0700 (MST)
+
+module.exports = function (config) {
+    config.set({
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
+
+
+        // list of files / patterns to load in the browser
+        files: [
+            'app/js/lib/local/jquery.js',
+            'app/js/lib/local/angular.js',
+            'app/js/lib/local/angular-route.js',
+            'app/js/lib/local/angular-strap.js',
+            'app/js/lib/local/angular-strap-tpl.js',
+            'app/js/lib/local/angular-animate.js',
+            'app/js/lib/local/bootstrap.js',
+            'app/js/lib/local/crossfilter.js',
+            'app/js/lib/local/lodash.js',
+            'app/js/lib/local/zxcvbn.js',
+            'app/js/lib/local/leaflet.js',
+            'app/js/lib/local/google-maps.js',
+            'app/js/app.js',
+            'tests/*.js'
+        ],
+
+        // list of files to exclude
+        exclude: [
+        ],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+//            'app/js/app.js': ['coverage']
+        },
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
+
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_DEBUG,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false
+    });
+};
