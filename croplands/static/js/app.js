3de30cbb56a8d2de9ec11b2d0d2b70676145397f
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-06-28 11:15:07 -0500

    renamed photos to images and fix url script for image directive

--- croplands/static/js/app.js
+++ croplands/static/js/app.js
@@ -6508,6 +6508,36 @@ app.directive('forgotForm', ['user', 'log', '$timeout', function (user, log, $ti
     };
 
 }]);;
+app.directive('images', [function () {
+    return {
+        restrict: 'E',
+        scope: {
+            items: '=items'
+        },
+        link: function (scope) {
+            scope.$watch('items', function (val) {
+                if (val && val.length > 0) {
+                    scope.active = scope.items[0];
+                }
+                else {
+                    scope.active = null;
+                }
+            });
+            scope.src = function (url) {
+                if(url) {
+                    // use first directory to make subdomain
+                    return "//images.croplands.org" + url.replace('images/','/');
+                }
+            };
+
+            scope.changeLeadPhoto = function (index) {
+                scope.active = scope.items[index];
+            };
+        },
+        templateUrl: '/static/directives/images.html'
+    };
+
+}]);;
 app.directive('legend', [function () {
     return {
         restrict: 'E',
@@ -7118,35 +7148,6 @@ app.directive('passwordConfirm', ['$window', function ($window) {
 }])
 ;
 ;
-app.directive('photos', [function () {
-    return {
-        restrict: 'E',
-        scope: {
-            items: '=items'
-        },
-        link: function (scope) {
-            scope.$watch('items', function (val) {
-                if (val && val.length > 0) {
-                    scope.active = scope.items[0];
-                }
-                else {
-                    scope.active = null;
-                }
-            });
-            scope.src = function (url) {
-                if(url) {
-                    return "https://s3.amazonaws.com/gfsad30/" + url;
-                }
-            };
-
-            scope.changeLeadPhoto = function (index) {
-                scope.active = scope.items[index];
-            };
-        },
-        templateUrl: '/static/directives/photos.html'
-    };
-
-}]);;
 app.directive('resetForm', ['user', '$window', '$timeout', function (user, $window, $timeout) {
     return {
         restrict: 'E',
