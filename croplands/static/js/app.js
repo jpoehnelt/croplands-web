ad8af681318db550601f2c2245a07bc00c55d061
Justin Poehnelt <justin.poehnelt@gmail.com>
2016-03-12 20:58:33 -0700

    missing countries for registration

--- croplands/static/js/app.js
+++ croplands/static/js/app.js
@@ -4731,7 +4731,9 @@ app.factory('RatingService', ['$http', '$rootScope', 'log', 'User', '$q','locati
 }]);;
 app.factory('User', [ '$http', '$window', '$q', 'log','$rootScope', function ($http, $window, $q, log, $rootScope) {
     var _user = {},
-      _baseUrl = 'https://api.croplands.org';
+      _baseUrl = 'http://127.0.0.1:8000';
+
+//      _baseUrl = 'http://api.croplands.org';
 
     function getUser() {
         return _user;
@@ -4843,7 +4845,7 @@ app.factory('User', [ '$http', '$window', '$q', 'log','$rootScope', function ($h
             deferred = $q.defer(),
             headers = { Accept: 'application/json', 'Content-Type': 'application/json'};
 
-        $http.post("https://api.croplands.org/auth/forgot", data, headers).then(function (r) {
+        $http.post(_baseUrl + "/auth/forgot", data, headers).then(function (r) {
                 log.info("[User] Sending reset email.");
                 deferred.resolve(r.data);
             },
@@ -4865,7 +4867,7 @@ app.factory('User', [ '$http', '$window', '$q', 'log','$rootScope', function ($h
             deferred = $q.defer(),
             headers = { Accept: 'application/json', 'Content-Type': 'application/json'};
 
-        $http.post("https://api.croplands.org/auth/reset", data, headers).then(function (r) {
+        $http.post(_baseUrl + "/auth/reset", data, headers).then(function (r) {
                 log.info("[User] Changing password.");
                 if (r.data.data.token) {
                     loadFromToken(r.data.data.token);
@@ -4940,6 +4942,48 @@ app.factory('User', [ '$http', '$window', '$q', 'log','$rootScope', function ($h
     };
 
 }]);;
+app.constant('countries', [
+        "Afghanistan", "Aland Islands", "Albania", "Algeria", "American Samoa", "Andorra", "Angola",
+        "Anguilla", "Antarctica", "Antigua And Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria",
+        "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin",
+        "Bermuda", "Bhutan", "Bolivia, Plurinational State of", "Bonaire, Sint Eustatius and Saba", "Bosnia and Herzegovina",
+        "Botswana", "Bouvet Island", "Brazil",
+        "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia",
+        "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
+        "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo",
+        "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba",
+        "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt",
+        "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)",
+        "Faroe Islands", "Fiji", "Finland", "France", "French Guiana", "French Polynesia",
+        "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece",
+        "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guernsey", "Guinea",
+        "Guinea-Bissau", "Guyana", "Haiti", "Heard Island and McDonald Islands", "Holy See (Vatican City State)",
+        "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran, Islamic Republic of", "Iraq",
+        "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya",
+        "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan",
+        "Lao People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya",
+        "Liechtenstein", "Lithuania", "Luxembourg", "Macao", "Macedonia, The Former Yugoslav Republic Of",
+        "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique",
+        "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of",
+        "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru",
+        "Nepal", "Netherlands", "New Caledonia", "New Zealand", "Nicaragua", "Niger",
+        "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau",
+        "Palestinian Territory, Occupied", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines",
+        "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation",
+        "Rwanda", "Saint Barthelemy", "Saint Helena, Ascension and Tristan da Cunha", "Saint Kitts and Nevis", "Saint Lucia",
+        "Saint Martin (French Part)", "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines", "Samoa", "San Marino",
+        "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore",
+        "Sint Maarten (Dutch Part)", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa",
+        "South Georgia and the South Sandwich Islands", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname",
+        "Svalbard and Jan Mayen", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic",
+        "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Timor-Leste",
+        "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan",
+        "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom",
+        "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu",
+        "Venezuela, Bolivarian Republic of", "Viet Nam", "Virgin Islands, British", "Virgin Islands, U.S.",
+        "Wallis and Futuna", "Western Sahara", "Yemen", "Zambia", "Zimbabwe"
+    ]);
+;
 app.factory('geoHelperService', [ function () {
     /**
      * Calculates the ending location given a lat lon pair, bearing and distance.
@@ -6715,7 +6759,7 @@ app.directive('passwordConfirm', ['$window', function ($window) {
 }]);
 
 ;
-app.directive('resetForm', ['user', '$window', '$timeout', function (user, $window, $timeout) {
+app.directive('resetForm', ['User', '$window', '$timeout', function (User, $window, $timeout) {
     return {
         restrict: 'E',
         scope: {
@@ -6729,7 +6773,7 @@ app.directive('resetForm', ['user', '$window', '$timeout', function (user, $wind
 
             scope.reset = function () {
                 scope.busy = true;
-                user.reset(scope.password, scope.token).then(function (response) {
+                User.reset(scope.password, scope.token).then(function (response) {
                     setMessage(response.description, true);
                     scope.busy = false;
                     scope.close();
