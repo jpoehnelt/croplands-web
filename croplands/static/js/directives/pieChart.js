9adfb7c92a0ac6101b6785aae980a5c4827ba539
Justin Poehnelt <justin.poehnelt@gmail.com>
2016-03-18 15:52:56 -0700

    cleanup

--- croplands/static/js/directives/pieChart.js
+++ croplands/static/js/directives/pieChart.js
@@ -5,7 +5,7 @@ app.directive('pieChart', ['$http', '$log', '$q',
             scope: {
                 value: '=value'
             },
-            link: function (scope, element, attributes) {
+            link: function (scope) {
                 var size = 100;
                 scope.radius = size / 2;
                 scope.background = '#cccccc';
@@ -21,26 +21,19 @@ app.directive('pieChart', ['$http', '$log', '$q',
 
                     value = Math.min(Math.max(value, 0), 100);
 
-                    console.log('pie value', value);
                     if (value === 100) {
-                        console.log('short circuit pie');
                         scope.background = '#237c28';
                         scope.d = '';
                         return;
                     }
 
-
-
-                    //calculate x,y coordinates of the point on the circle to draw the arc to.
                     var x = Math.cos((2 * Math.PI) / (100 / value));
                     var y = Math.sin((2 * Math.PI) / (100 / value));
 
                     //should the arc go the long way round?
                     var longArc = (value <= 50) ? 0 : 1;
 
-                    //d is a string that describes the path of the slice.
                     scope.d = "M" + scope.radius + "," + scope.radius + " L" + scope.radius + ", 0, A" + scope.radius + "," + scope.radius + " 0 " + longArc + ",1 " + (scope.radius + y * scope.radius) + "," + (scope.radius - x * scope.radius) + " z";
-
                 });
 
 
