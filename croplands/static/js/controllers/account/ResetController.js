0c70edfdff6f4db0a916e0ca9b4c5c2c07643f5b
Justin Poehnelt <justin.poehnelt@gmail.com>
2016-04-13 13:14:37 -0700

    add passwords back

--- croplands/static/js/controllers/account/ResetController.js
+++ croplands/static/js/controllers/account/ResetController.js
@@ -1,15 +1,29 @@
-app.controller("ResetController", ['$location', '$scope', 'log', '$window', function ($location, $scope, log, $window) {
+app.controller("ResetController", ['$location', '$scope', '$window', 'User', function ($location, $scope, $window, User) {
     $scope.token = $location.search().token;
-    var path = $location.path().split('/');
 
-    if (path[1] === 'account') {
-        $scope.$emit('user.' + path[2], true);
-    } else {
-        log.info('not account/reset');
-//        $window.location.href = '/';
+    function setMessage(message, success) {
+        $scope.success = success;
+        $scope.message = message;
     }
-    if ($scope.token === undefined) {
-        log.warn('Token not found');
-//        $window.location.href = '/';
-    }
+
+    $scope.reset = function () {
+        $scope.busy = true;
+        User.reset($scope.password, $scope.token).then(function (response) {
+            setMessage(response.description, true);
+            $scope.busy = false;
+            $scope.close();
+        }, function (response) {
+            if (response.description) {
+                setMessage(response.description, false);
+            }
+            else {
+                setMessage('Something went wrong', false);
+            }
+            $scope.busy = false;
+        });
+    };
+
+    $scope.close = function () {
+        $window.location.href = '/';
+    };
 }]);
